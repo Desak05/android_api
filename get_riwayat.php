5b394175dfb3bcba085c539cf36da12b72a0deda
Desak05 <desakdiahsetyawati@gmail.com>
2025-07-05 16:34:33 +0800

    Update kode terbaru

--- get_riwayat.php
+++ get_riwayat.php
@@ -1,8 +1,10 @@
 <?php
 include 'koneksi.php';
 
-// Ambil seluruh riwayat pesanan dan informasi nama makanan
-$query = "SELECT menu_makanan.nama AS nama_makanan, 
+header('Content-Type: application/json');
+
+// Query untuk ambil data riwayat dan nama makanan
+$query = "SELECT rp.id, rp.id_makanan, menu_makanan.nama AS nama_makanan, 
                  rp.jumlah, rp.total_harga 
           FROM riwayat_pesanan rp 
           JOIN menu_makanan ON rp.id_makanan = menu_makanan.id
@@ -10,10 +12,24 @@ $query = "SELECT menu_makanan.nama AS nama_makanan,
 
 $result = mysqli_query($conn, $query);
 
-$data = array();
-while ($row = mysqli_fetch_assoc($result)) {
-    $data[] = $row;
-}
+$response = [];
 
-echo json_encode($data);
+if ($result) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        $response[] = [
+            'id' => (int) $row['id'],
+            'id_makanan' => (int) $row['id_makanan'],
+            'nama_makanan' => $row['nama_makanan'],
+            'jumlah' => (int) $row['jumlah'],
+            'total_harga' => (int) $row['total_harga']
+        ];
+    }
+
+    echo json_encode($response);
+} else {
+    echo json_encode([
+        'success' => false,
+        'message' => 'Gagal mengambil data: ' . mysqli_error($conn)
+    ]);
+}
 ?>
